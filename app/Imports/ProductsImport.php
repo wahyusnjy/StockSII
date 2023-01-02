f97e0b515711b7c41b0a75fdb966cc746f904a7f
MacBook <wahyusnjy@gmail.com>
2023-01-02 09:02:36 +0700

    Product Import

--- app/Imports/ProductsImport.php
+++ app/Imports/ProductsImport.php
@@ -21,32 +21,51 @@ class ProductsImport implements ToModel, WithHeadingRow
     public function model(array $row)
     {
 
+        // $inputall = $row->all();
         $input = $row['no'];
         $code = $row['nama'];
         $get_category = Category::orderBy('name','ASC')
         ->where('id', $row["category"])->first();
-        //dd($get_category);
+        //  dd($get_category);
         $lokasi       = Lokasi::orderBy('name','ASC')
         ->where('id',$row["lokasi"])->first();
+        $productname = Product::where('nama', $row['nama'])->first();
+        //dd($row['category_name']);
 
         $input = strtoupper("Product :".$row['nama'])."\n".strtoupper("Lokasi : ". $row['lokasi_name'])."\n".strtoupper("Category : ".$row['category_name']);
         $prefix = '.';
         $id = $row['no'];
         $test = str_pad($id,5,'0', STR_PAD_LEFT);
-        $qrcode = strtoupper(substr($get_category->name, 0, 1).substr($get_category->name, 6, 1)).$test;
-
-       return  $new = Product::updateOrCreate([
-            'id'            => $row['no'],
-            'nama'          => $row['nama'],
-            'harga'         => $row['harga'],
-            'qty'           => $row['qty'],
-            'category_id'   => $row['category'],
-            'lokasi_id'     => $row['lokasi'],
-            'assets_id'     => $row['assets'],
-            'user'          => $row['user'],
-            'product_code'  => $input,
-            'qrcode'        => $qrcode
-        ]);
+        $qrcode = strtoupper(substr($row['category_name'] , 0, 1).substr($row['category_name'], 6, 1)).$test;
+        $productqty = Product::orderBy('qty', 'ASC')->first();
+        $newqty = $productqty->qty + $row['qty'];
+       //dd($get_category->name);
+    //    if($row['nama'] == null){
+    //     dd($row['nama']);
+    //    }
+        return  $new = Product::updateOrCreate(
+            [
+            'nama' => $row['nama'] ?? 'Not Found',
+            'qty'           => $newqty ?? $row['qty'] ?? 0,
+            'lokasi_id'     => $row['lokasi'] ?? 71,
+            'product_code'  => $input ?? '404 Not Found',
+            'qrcode'        => $qrcode ?? '404 Not Found',
+            ],[
+                // 'id'            => $row['no'],
+                // 'harga'         => $row['harga'],
+                // 'qty'           => $row['qty'],
+                // 'user'          => $row['user'],
+                // 'product_code'  => $input,
+                // 'qrcode'        => $qrcode,
+                // 'category_id'   => $row['category'],
+                // 'lokasi_id'     => $row['lokasi'],
+                // 'assets_id'     => $row['assets'],
+                'id'            => $row['no'] ?? 404,
+                'harga'         => $row['harga'] ?? 0,
+                'user'          => $row['user'] ?? 'Tidak Ditemukan',
+                'category_id'   => $row['category'] ?? 5,
+                'assets_id'     => $row['assets'] ?? 3,
+            ]);
         // return new Product([
         //     // 'id'            => $row['no'],
         //     // 'nama'          => $row['nama'],
